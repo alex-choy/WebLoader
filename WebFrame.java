9d4f215ef4af178be78cc804aaa17f9cecaf4ed9
b0kch0yb0i <alexchoy2345@gmail.com>
2016-11-19 16:51:39 -0800

    Almost Done
    Got the URLs loading, buttons working. Now I just need to make the
    buttons reset without having to press the stop button

--- WebFrame.java
+++ WebFrame.java
@@ -1,3 +1,4 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -6,7 +7,10 @@
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Semaphore;
 
 import javafx.application.Application;
@@ -33,144 +37,185 @@ import javafx.stage.Stage;
  * @author narayan
  */
 public class WebFrame extends Application {
-    
-    @SuppressWarnings("unchecked")
-	@Override
-    public void start(Stage stage) throws FileNotFoundException {
-        TableView<URLStrings> tv = new TableView();
-        Semaphore sem = new Semaphore(4);
-        Scanner in = new Scanner(new File("URL.txt"));
-        double count = 0;       
-        
-        ObservableList<URLStrings> data = FXCollections.observableArrayList();
-       
-        while (in.hasNextLine()){
-        	String url = in.next();
-        	String status = "";
-        	
-        	URLStrings string = new URLStrings(url, status);
-        	data.add(string);
-        	count++; //Increment count so we can update the status bar
-        }
-        double addToProgress = 1/count;
-        in.close();
-        
-        TableColumn URLCol = new TableColumn("URL");
-        URLCol.setMinWidth(100);
-        URLCol.setCellValueFactory(
-                new PropertyValueFactory<URLStrings, String>("URL"));
-        
-        Button fetchButt = new Button("Fetch");
-        Button stopButt  = new Button("Stop");
-        stopButt.setDisable(true);
 
-        
-        ProgressBar bar = new ProgressBar(0);
-		//WebWorker worker = new WebWorker("http://www.sjsu.edu/cs/");
-        
-        	
-        
-        
-        TableColumn statusCol = new TableColumn("Status");
-        statusCol.setMinWidth(100);
-        statusCol.setCellValueFactory(
-                new PropertyValueFactory<URLStrings, String>("status"));
-        
-        tv.getColumns().addAll(URLCol, statusCol);
-        tv.setItems(data);
-        tv.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
-        
-        fetchButt.setOnAction(new EventHandler(){
+	@SuppressWarnings("unchecked")
+	@Override
+	public void start(Stage stage) throws FileNotFoundException {
+		ObservableList<URLStrings> data = FXCollections.observableArrayList();
+		TableView<URLStrings> tv = new TableView<URLStrings>();
+		double count = 0;
+		int runningThreadsCount = 0;
+		
+		Button fetchButt = new Button("Fetch");
+		Button multiButt = new Button("Multiple Fetch");
+		Button stopButt = new Button("Stop");
+		stopButt.setDisable(true);
+		
+		List<Thread> theWorkers = new ArrayList<Thread>();
+
+		TableColumn URLCol = new TableColumn("URL");
+		URLCol.setMinWidth(100);
+		URLCol.setCellValueFactory(new PropertyValueFactory<URLStrings, String>("URL"));
+
+		
+
+		ProgressBar bar = new ProgressBar(0);
+
+		Scanner in = new Scanner(new File("URL.txt"));
+
+
+		while (in.hasNextLine()) {
+			String url = in.nextLine();
+			String status = "";
+
+			URLStrings string = new URLStrings(url, status);
+			data.add(string);
+			count++; // Increment count so we can update the status bar
+		}
+		double addToProgress = 1 / count;
+		final int numOfURLs = (int) count;
+		in.close();
+		
+		TableColumn statusCol = new TableColumn("Status");
+		statusCol.setMinWidth(100);
+		statusCol.setCellValueFactory(new PropertyValueFactory<URLStrings, String>("status"));
+
+		tv.getColumns().addAll(URLCol, statusCol);
+		tv.setItems(data);
+		tv.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+
+		fetchButt.setOnAction(new EventHandler() {
 			@Override
 			public void handle(Event event) {
-				fetchButt.setDisable(true);
-				stopButt.setDisable(false);
-				WebWorker worker;
-				for(URLStrings url: data){
-					worker = new WebWorker(url.getURL().toString());
-					url.setStatus("Running");
-					tv.getColumns().get(1).setVisible(false);
-					tv.getColumns().get(1).setVisible(true);
-					try {
-						sem.acquire();
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					worker.start();
-					sem.release();
-					double progress = bar.getProgress();
-					
-			        bar.setProgress(bar.getProgress() + addToProgress);
+				runningState(fetchButt, multiButt, stopButt);
+				
+				Semaphore sem1 = new Semaphore(1);
+				CountDownLatch count = new CountDownLatch(numOfURLs);
+
+				try {
+					startWorkers(tv, data, theWorkers, sem1, addToProgress, bar, count);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
 				}
+			}
+
+		});
+
+		multiButt.setOnAction(new EventHandler() {
+			@Override
+			public void handle(Event event) {
+				runningState(fetchButt, multiButt, stopButt);
+				Semaphore sem = new Semaphore(4);
+				CountDownLatch count = new CountDownLatch(numOfURLs);
+				try {
+					startWorkers(tv, data, theWorkers, sem, addToProgress, bar, count);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+
+		});
+
+		stopButt.setOnAction(new EventHandler() {
+			@Override
+			public void handle(Event event) {
+				for(Thread worker: theWorkers){
+					
+					worker.interrupt(); 	
+				}
+				readyState(fetchButt, multiButt, stopButt, bar);
 				
 			}
-        		
-        });
-        
-        stopButt.setOnAction(new EventHandler(){
-        	@Override	
-        	public void handle(Event event){
-        		stopButt.setDisable(true);
-        		for(URLStrings s: data){
-        			s.setStatus("Stopped");
-        		}
-				tv.getColumns().get(1).setVisible(false);
-				tv.getColumns().get(1).setVisible(true);
-        		
-        		//worker.interrupt();
-        		//fetchButt.setDisable(false);
-        	}
-        });
-        
-        Scene scene = new Scene(new Group());
-        stage.setTitle("Table View Sample");
-        stage.setHeight(500);
-        
-        final VBox vbox = new VBox();
-        vbox.setSpacing(5);
-        vbox.setPadding(new Insets(10, 0, 0, 10));
-        
-        HBox hbox = new HBox();
-        hbox.setSpacing(5);
-        hbox.setPadding(new Insets(10, 0, 0, 10));
 
-        hbox.getChildren().addAll(fetchButt, stopButt, bar);
-        vbox.getChildren().addAll(tv,hbox);
-        ((Group) scene.getRoot()).getChildren().addAll(vbox);
- 
-        stage.setScene(scene);
-        stage.show();
-        
-      
-    }
+		});
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        launch(args);
-    }
-    
+		Scene scene = new Scene(new Group());
+		stage.setTitle("Table View Sample");
+		stage.setHeight(500);
+		stage.setWidth(335);
 
+		final VBox vbox = new VBox();
+		vbox.setSpacing(5);
+		vbox.setPadding(new Insets(10, 0, 0, 10));
 
-    public static class URLStrings {
-        private StringProperty URL;
-        private StringProperty status;
-        URLStrings(String URL, String status) {
-            this.URL = new SimpleStringProperty(URL);
-            this.status = new SimpleStringProperty(status);
-        }
-        public String getURL() {
-            return URL.get();
-        }
+		HBox hbox = new HBox();
+		hbox.setSpacing(5);
+		hbox.setPadding(new Insets(10, 0, 0, 10));
 
-        public String getStatus() {
-            return status.get();
-        }
-        
-        public void setStatus(String status){
-        	this.status = new SimpleStringProperty(status);
-        }
-    }
+		hbox.getChildren().addAll(fetchButt, multiButt, stopButt, bar);
+		vbox.getChildren().addAll(tv, hbox);
+		((Group) scene.getRoot()).getChildren().addAll(vbox);
+
+		stage.setScene(scene);
+		stage.show();
+
+	}
+
+	/**
+	 * @param args
+	 *            the command line arguments
+	 */
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	public static class URLStrings {
+		private StringProperty URL;
+		private StringProperty status;
+
+		URLStrings(String URL, String status) {
+			this.URL = new SimpleStringProperty(URL);
+			this.status = new SimpleStringProperty(status);
+			// this.runningThreadsCount = runningThreadsCount;
+		}
+
+		public String getURL() {
+			return URL.get();
+		}
+
+		public String getStatus() {
+			return status.get();
+		}
+
+		public void setStatus(String status) {
+			this.status = new SimpleStringProperty(status);
+		}
+
+	}
+
+	private static void startWorkers(TableView tv, ObservableList<URLStrings> data, List<Thread> theWorkers, Semaphore sem1,
+			double addToProgress, ProgressBar bar, CountDownLatch count) throws InterruptedException {
+		
+		for (int i = 0; i < data.size(); i++) {
+			
+			URLStrings url = data.get(i);
+			WebWorker worker = new WebWorker(data, url.getURL(), i, addToProgress, bar, tv,count);
+			url.setStatus("Running");
+			tv.refresh();
+			Thread t1 = new Thread(worker);
+			theWorkers.add(t1);
+			try {
+				sem1.acquire();
+				t1.start();
+				
+				sem1.release();
+			} catch (Exception e) {
+
+			}
+		}
+	}
+	private static void runningState(Button fetchButt, Button multiButt, Button stopButt){
+		fetchButt.setDisable(true);
+		multiButt.setDisable(true);
+		stopButt.setDisable(false);
+	}
+	
+	private static void readyState(Button fetchButt, Button multiButt, Button stopButt, ProgressBar bar){
+		fetchButt.setDisable(false);
+		multiButt.setDisable(false);
+		stopButt.setDisable(true);
+		bar.setProgress(0);
+		
+		
+	}
 }
--- WebWorker.java
+++ WebWorker.java
@@ -2,32 +2,61 @@ import java.io.*;
 import java.net.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.CountDownLatch;
+
 import javax.swing.*;
 
+import javafx.collections.ObservableList;
+import javafx.scene.control.Button;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TableView;
+
 public class WebWorker extends Thread {
-/*
-  This is the core web/download i/o code...*/
-     String urlString;
-     public WebWorker(String url) {
-     	urlString = url;
-     }
-     public void run() {
-     	System.out.println("Fetching...." + urlString);
- 		InputStream input = null;
+	/*
+	 * This is the core web/download i/o code...
+	 */
+	ObservableList<WebFrame.URLStrings> data;
+	String urlString;
+	int rowNumber;
+	boolean interrupted;
+	ProgressBar bar;
+	double incrementBar;
+	TableView tv;
+	// Button fetchButt;
+	// Button multiButt;
+	// Button stopButt;
+	CountDownLatch count;
+
+	public WebWorker(ObservableList<WebFrame.URLStrings> data, String url, int rowNumber, double incrementBar,
+			ProgressBar bar, TableView tv, CountDownLatch count) {
+		this.data = data;
+		urlString = url;
+		this.rowNumber = rowNumber;
+		interrupted = false;
+		this.bar = bar;
+		this.incrementBar = incrementBar;
+		this.tv = tv;
+		this.count = count;
+
+	}
+
+	public synchronized void run() {
+		System.out.println("Fetching...." + urlString);
+		InputStream input = null;
 		StringBuilder contents = null;
 		try {
 			URL url = new URL(urlString);
 			URLConnection connection = url.openConnection();
-		
+
 			// Set connect() to throw an IOException
 			// if connection does not succeed in this many msecs.
 			connection.setConnectTimeout(5000);
-			
+
 			connection.connect();
 			input = connection.getInputStream();
 
-			BufferedReader reader  = new BufferedReader(new InputStreamReader(input));
-		
+			BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
 			char[] array = new char[1000];
 			int len;
 			contents = new StringBuilder(1000);
@@ -36,37 +65,41 @@ public class WebWorker extends Thread {
 				contents.append(array, 0, len);
 				Thread.sleep(100);
 			}
-			
+
 			System.out.print(contents.toString());
-			
+
 		}
 		// Otherwise control jumps to a catch...
-		catch(MalformedURLException ignored) {
+		catch (MalformedURLException ignored) {
 			System.out.println("Exception: " + ignored.toString());
-		}
-		catch(InterruptedException exception) {
+		} catch (InterruptedException exception) {
 			// YOUR CODE HERE
 			// deal with interruption
-			System.out.println("The program has been interrupted" );
-			System.out.println("Exception: " + exception.toString());
-		}
-		catch(IOException ignored) {
+			System.out.println("The program has been interrupted");
+			interrupted = true;
+		} catch (IOException ignored) {
 			System.out.println("Exception: " + ignored.toString());
 		}
 		// "finally" clause, to close the input stream
 		// in any case
 		finally {
-			try{
-				if (input != null) input.close();
+			try {
+				if (input != null)
+					input.close();
+			} catch (IOException ignored) {
 			}
-			catch(IOException ignored) {}
 		}
-	}
-     
-     
-     
-     public void stopThread(){
-     }
+		if (!interrupted) {
+			bar.setProgress(bar.getProgress() + incrementBar);
+			data.get(rowNumber).setStatus("Complete");
+			tv.refresh();
+			count.countDown();
+		}
+		else{
+			data.get(rowNumber).setStatus("Interrupted");
+			tv.refresh();
+		}
 
-	
+	}
+
 }
